90ae1c9a4cf735ca2785455acd81b0bdff500ef2
孙颢 <sunhao@bestminr.com>
2018-09-17 15:49:03 +0800

    feature/init: add ssr plugin

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createRouter } from './router'
+import { createStore } from './store'
 import './plugins/element'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+export async function createApp({
+  context = null,
+  beforeApp = (() => {}) as any,
+  afterApp = (() => {}) as any
+} = {}) {
+  const router = createRouter()
+  const store = createStore()
+
+  await beforeApp({
+    router,
+    store
+  })
+
+  const app = new Vue({
+    router,
+    store,
+    render: h => h(App)
+  })
+
+  const result = {
+    app,
+    router,
+    store
+  }
+
+  await afterApp(result)
+
+  return result
+}
